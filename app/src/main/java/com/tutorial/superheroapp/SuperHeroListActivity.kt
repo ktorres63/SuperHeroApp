eaf8095170d1a0cd91e36ba9ee19ef478f3428e4
ktorres63 <ktorresaro@unsa.edu.pe>
2023-11-12 17:25:50 -0500

    using viewBinding

--- app/src/main/java/com/tutorial/superheroapp/SuperHeroListActivity.kt
+++ app/src/main/java/com/tutorial/superheroapp/SuperHeroListActivity.kt
@@ -2,10 +2,32 @@ package com.tutorial.superheroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+import com.tutorial.superheroapp.databinding.ActivitySuperHeroListBinding
 
 class SuperHeroListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySuperHeroListBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_super_hero_list)
+
+        initUI()
+
+    }
+
+    private fun initUI() {
+        binding.searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                return false
+              
+            }
+
+            override fun onQueryTextChange(newText: String?) = false
+
+        })
     }
 }
