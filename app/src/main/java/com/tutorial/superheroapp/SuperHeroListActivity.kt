2f968193fb023e32e20d6ce1b32f8e91f1bf45fc
ktorres63 <ktorresaro@unsa.edu.pe>
2023-11-12 17:14:39 -0500

    create the searchView and reciclerView

---
+++ app/src/main/java/com/tutorial/superheroapp/SuperHeroListActivity.kt
@@ -0,0 +1,11 @@
+package com.tutorial.superheroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuperHeroListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_super_hero_list)
+    }
+}
