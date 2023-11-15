6f72d9d6852259560fbe04a62c9f666a88830734
ktorres63 <ktorresaro@unsa.edu.pe>
2023-11-14 21:25:08 -0500

    add images to App

--- app/src/main/java/com/tutorial/superheroapp/SuperheroViewHolder.kt
+++ app/src/main/java/com/tutorial/superheroapp/SuperheroViewHolder.kt
@@ -2,13 +2,16 @@ package com.tutorial.superheroapp
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import com.tutorial.superheroapp.databinding.ItemSuperheroBinding
 
-class SuperheroViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class SuperheroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val binding = ItemSuperheroBinding.bind(view)
-    fun bind(superHeroesItemResponse: SuperHeroesItemResponse){
+    fun bind(superHeroesItemResponse: SuperHeroesItemResponse) {
         binding.tvSuperheroName.text = superHeroesItemResponse.name
 
+
+        Picasso.get().load(superHeroesItemResponse.superHeroImage.url).into(binding.ivSuperHero)
     }
 
 
