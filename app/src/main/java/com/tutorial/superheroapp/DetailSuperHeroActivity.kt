bd3e4c2cb6acf0163e74b3aab6ca9a129e37815d
ktorres63 <ktorresaro@unsa.edu.pe>
2023-11-21 14:19:19 -0500

    add bar graphic to App

--- app/src/main/java/com/tutorial/superheroapp/DetailSuperHeroActivity.kt
+++ app/src/main/java/com/tutorial/superheroapp/DetailSuperHeroActivity.kt
@@ -2,6 +2,8 @@ package com.tutorial.superheroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
 import com.squareup.picasso.Picasso
 import com.tutorial.superheroapp.databinding.ActivityDetailSuperHeroBinding
 import kotlinx.coroutines.CoroutineScope
@@ -9,6 +11,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.roundToInt
 
 class DetailSuperHeroActivity : AppCompatActivity() {
     companion object {
@@ -30,8 +33,8 @@ class DetailSuperHeroActivity : AppCompatActivity() {
             val superheroDetail =
                 getRetrofit().create(ApiService::class.java).getSuperheroeDetail(id)
 
-            if(superheroDetail.body() != null){
-                runOnUiThread{
+            if (superheroDetail.body() != null) {
+                runOnUiThread {
                     createUI(superheroDetail.body()!!)
                 }
 
@@ -42,6 +45,28 @@ class DetailSuperHeroActivity : AppCompatActivity() {
 
     private fun createUI(superHero: SuperHeroDetailResponse) {
         Picasso.get().load(superHero.image.url).into(binding.ivSuperHero)
+        binding.tvSuperheroName.text = superHero.name
+        prepareStats(superHero.powerstats)
+    }
+
+    private fun prepareStats(powerstats: PowerStatsResponse) {
+
+        updateHeight(binding.viewCombat, powerstats.combat)
+        updateHeight(binding.viewDurability, powerstats.durability)
+        updateHeight(binding.viewSpeed, powerstats.speed)
+        updateHeight(binding.viewIntelligence, powerstats.intelligence)
+        updateHeight(binding.viewStrength, powerstats.strength)
+        updateHeight(binding.viewPower, powerstats.power)
+    }
+
+    private fun updateHeight(view: View, stat: String) {
+        val params = view.layoutParams
+        params.height = pxToDp(stat.toFloat())
+        view.layoutParams = params
+    }
+
+    private fun pxToDp(px: Float): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, resources.displayMetrics).roundToInt()
     }
 
     private fun getRetrofit(): Retrofit {
@@ -51,6 +76,25 @@ class DetailSuperHeroActivity : AppCompatActivity() {
             .build()
 
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
